d5e97045247b407e2ab382a631b71ab922f8f7af
VitorCapuano <capuano.vitor@gmail.com>
2017-10-01 14:22:17 -0300

    Enable white noise

--- proxy/proxy/settings.py
+++ proxy/proxy/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +70,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'proxy.wsgi.application'
 
